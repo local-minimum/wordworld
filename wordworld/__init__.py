b29a0b2b7e9040460f48b352d9f0071eeebeaea3
Martin Zackrisson <martin.zackrisson@fripost.org>
2022-01-28 20:44:43 +0100

    Added svenska ord

--- wordworld/__init__.py
+++ wordworld/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask, send_from_directory, request, jsonify
+_CAP_WORD_LENGTH = 10
 
 with open('/app/wordworld/words.txt', 'r') as fh:
-    all_words = {w.strip() for w in fh.readlines()}
+    all_words = {
+        w.strip() for w in fh.readlines()
+        if len(w) <= _CAP_WORD_LENGTH
+    }
+
+with open('/app/wordworld/ord.txt', 'r') as fh:
+    alla_ord = {
+        w.strip() for w in fh.readlines()
+        if len(w) <= _CAP_WORD_LENGTH
+    }
 
 app = Flask(__name__)
 
@@ -26,3 +36,9 @@ def send_home():
 def check_words():
     words = request.get_json()
     return jsonify([word.upper() in all_words for word in words])
+
+
+@app.route('/kolla', methods=["POST"])
+def kolla_ord():
+    words = request.get_json()
+    return jsonify([word.upper() in alla_ord for word in words])
